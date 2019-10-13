c7dacbf4b2a3d7e4e27648de4848cf4f0417bc53
Sarin Wanichwasin <sarin_beam30@hotmail.com>
2019-10-13 10:29:15 +0700

    Add register.page and modify navbar

--- JavaScript/main-page-javascript.js
+++ JavaScript/main-page-javascript.js
@@ -58,44 +58,64 @@ function carousel_promotion() {
 
 
 var slideIndex_slideshow_container = [1, 1];
-      var slideId = ["mySlides_slideshow_beam"];
-      var myIndex = 0;
-      carousel();
-      showSlides_slideshow_beam(1, 0);
-      showSlides_slideshow_beam(1, 1);
-      function plusSlides_slideshow_container(n, no) {
-        showSlides_slideshow_beam(
-          (slideIndex_slideshow_container[no] += n),
-          no
-        );
-      }
-      function showSlides_slideshow_beam(n, no) {
-        var i;
-        var x = document.getElementsByClassName(slideId[no]);
-        if (n > x.length) {
-          slideIndex_slideshow_container[no] = 1;
-        }
-        if (n < 1) {
-          slideIndex_slideshow_container[no] = x.length;
-        }
-        for (i = 0; i < x.length; i++) {
-          x[i].style.display = "none";
-        }
+var slideId = ["mySlides_slideshow_beam"];
+var myIndex = 0;
+carousel();
+showSlides_slideshow_beam(1, 0);
+showSlides_slideshow_beam(1, 1);
+function plusSlides_slideshow_container(n, no) {
+  showSlides_slideshow_beam(
+    (slideIndex_slideshow_container[no] += n),
+    no
+  );
+}
+function showSlides_slideshow_beam(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {
+    slideIndex_slideshow_container[no] = 1;
+  }
+  if (n < 1) {
+    slideIndex_slideshow_container[no] = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
 
-        if (no !== 1) {
-          x[slideIndex_slideshow_container[no] - 1].style.display = "block";
-        }
-      }
-      function carousel() {
-        var i;
-        var x = document.getElementsByClassName("mySlides_slideshow_beam");
-        for (i = 0; i < x.length; i++) {
-          x[i].style.display = "none";
-        }
-        myIndex++;
-        if (myIndex > x.length) {
-          myIndex = 1;
-        }
-        x[myIndex - 1].style.display = "block";
-        setTimeout(carousel, 3000);
-      }
+  if (no !== 1) {
+    x[slideIndex_slideshow_container[no] - 1].style.display = "block";
+  }
+}
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides_slideshow_beam");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
+
+//----------------------------- Function Hide Menu on Scroll --------------------------------------------
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  // this.console.log("currentScrollPos : " + currentScrollPos);
+  
+  if(currentScrollPos === 0){
+    document.getElementById("panel-header-hide-menu-on-scroll").style.top = "0";
+    document.getElementById("header-sticky").style.marginTop = "30px";
+  }
+
+  else{
+    document.getElementById("panel-header-hide-menu-on-scroll").style.top = "-30px";
+    document.getElementById("header-sticky").style.marginTop = "0";
+  }
+
+  prevScrollpos = currentScrollPos;
+ 
+}
